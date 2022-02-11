78b02a9dc91319b330ff90578537ad7c22d180c9
Congzhiwei1994 <z1039329737@qq.com>
2022-02-11 21:44:45 +0800

    阿昊修改

---
+++ _proj/Assets/3rd/Toolkit/BaseToolKit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseToolKit
+{
+    public abstract GUIContent Content();
+
+    public virtual GUIStyle Style() { return new GUIStyle("ToolbarSeachCancelButton"); }
+
+    public virtual void OnAwake() { }
+
+    public abstract void OnGUI();
+
+    public virtual void OnDestroy() { }
+}
---
+++ _proj/Assets/3rd/Toolkit/ToolKits/CTool2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CTool2 : BaseToolKit
+{
+    public override GUIContent Content()
+    {
+        return new GUIContent(this.ToString());
+    }
+
+    public override void OnGUI()
+    {
+        
+    }
+
+}
