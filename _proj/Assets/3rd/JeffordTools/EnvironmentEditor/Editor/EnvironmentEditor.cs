d3517d59dc511250cdfb343cb18fce525f0dbb03
Congzhiwei1994 <z1039329737@qq.com>
2022-03-16 00:05:09 +0800

    11

--- _proj/Assets/3rd/JeffordTools/EnvironmentEditor/Editor/EnvironmentEditor.cs
+++ _proj/Assets/3rd/JeffordTools/EnvironmentEditor/Editor/EnvironmentEditor.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using Scene = UnityEngine.SceneManagement.Scene;
 
 namespace Jefford.EnvironmentEditor
 {
     public class EnvironmentEditor : EditorWindow
     {
+        private const string EnvRootName = "Environment";
         private static string m_currentScenePath;
         private EnvironmentStatus m_status;
 
@@ -19,7 +21,8 @@ namespace Jefford.EnvironmentEditor
             var window = GetWindow<EnvironmentEditor>("环境编辑器");
             window.Show();
 
-            m_currentScenePath = SceneManager.GetActiveScene().path;
+            m_currentScenePath = EditorSceneManager.GetActiveScene().path;
+
         }
 
         private void OnEnable()
@@ -30,6 +33,7 @@ namespace Jefford.EnvironmentEditor
         private void InitWindow()
         {
             InitView();
+            InitStatus();
         }
         private void InitView()
         {
@@ -39,6 +43,38 @@ namespace Jefford.EnvironmentEditor
         private void InitStatus()
         {
             m_status = new EnvironmentStatus();
+            Scene activeScene = EditorSceneManager.GetActiveScene();
+            var go = FindSceneObject(activeScene, EnvRootName);
+            if (go == null)
+            {
+                go = new GameObject(EnvRootName);
+            }
+            var env = GetOrAddComponent<Environment>(go);
+
+            m_status.Init();
+        }
+
+        private GameObject FindSceneObject(Scene scene, string name)
+        {
+            var rootGOs = scene.GetRootGameObjects();
+            foreach (var go in rootGOs)
+            {
+                if (go.name == name)
+                {
+                    return go;
+                }
+            }
+            return null;
+        }
+
+        private T GetOrAddComponent<T>(GameObject gameObject) where T : Component
+        {
+            T component = gameObject.GetComponent<T>();
+            if (component == null)
+            {
+                component = gameObject.AddComponent<T>();
+            }
+            return component;
         }
     }
 }
--- _proj/Assets/3rd/JeffordTools/EnvironmentEditor/Editor/EnvironmentStatus.cs
+++ _proj/Assets/3rd/JeffordTools/EnvironmentEditor/Editor/EnvironmentStatus.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using Scene = UnityEngine.SceneManagement.Scene;
 
 namespace Jefford.EnvironmentEditor
 {
