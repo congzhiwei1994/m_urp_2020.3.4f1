46619761b50504edaa530f6357f43031c578960b
Congzhiwei1994 <z1039329737@qq.com>
2022-05-05 23:43:57 +0800

    1111

--- m_urp_2020.3.4f1_proj/Assets/3rd/JeffordTools/C#/CSharp.cs
+++ m_urp_2020.3.4f1_proj/Assets/3rd/JeffordTools/C#/CSharp.cs
@@ -6,12 +6,14 @@ namespace Jefford.Csharp
 {
     public class CSharp : MonoBehaviour
     {
-        Enemy m_enemy;
-        Boss m_boss;
+        Genericity<int> m_gen;
+
+
         [ContextMenu("测试", false, 0)]
         void Test()
         {
-            m_boss = new Boss(2, 3);
+            m_gen = new Genericity<int>(1, 3);
+            m_gen.GetSum();
         }
 
     }
